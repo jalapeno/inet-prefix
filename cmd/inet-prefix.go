f161cafaed964b4de684da10582a284f0f4aab81
brmcdoug <brmcdoug@cisco.com>
2023-11-20 15:15:03 -0800

    inet

--- cmd/inet-prefix.go
+++ cmd/inet-prefix.go
@@ -40,17 +40,17 @@ var (
 
 func init() {
 	runtime.GOMAXPROCS(1)
-	// flag.StringVar(&msgSrvAddr, "message-server", "", "URL to the messages supplying server")
-	// flag.StringVar(&dbSrvAddr, "database-server", "", "{dns name}:port or X.X.X.X:port of the graph database")
-	// flag.StringVar(&dbName, "database-name", "", "DB name")
-	// flag.StringVar(&dbUser, "database-user", "", "DB User name")
-	// flag.StringVar(&dbPass, "database-pass", "", "DB User's password")
+	flag.StringVar(&msgSrvAddr, "message-server", "", "URL to the messages supplying server")
+	flag.StringVar(&dbSrvAddr, "database-server", "", "{dns name}:port or X.X.X.X:port of the graph database")
+	flag.StringVar(&dbName, "database-name", "", "DB name")
+	flag.StringVar(&dbUser, "database-user", "", "DB User name")
+	flag.StringVar(&dbPass, "database-pass", "", "DB User's password")
 
-	flag.StringVar(&msgSrvAddr, "message-server", "198.18.133.103:30092", "URL to the messages supplying server")
-	flag.StringVar(&dbSrvAddr, "database-server", "http://198.18.133.103:30852", "{dns name}:port or X.X.X.X:port of the graph database")
-	flag.StringVar(&dbName, "database-name", "jalapeno", "DB name")
-	flag.StringVar(&dbUser, "database-user", "root", "DB User name")
-	flag.StringVar(&dbPass, "database-pass", "jalapeno", "DB User's password")
+	// flag.StringVar(&msgSrvAddr, "message-server", "198.18.133.103:30092", "URL to the messages supplying server")
+	// flag.StringVar(&dbSrvAddr, "database-server", "http://198.18.133.103:30852", "{dns name}:port or X.X.X.X:port of the graph database")
+	// flag.StringVar(&dbName, "database-name", "jalapeno", "DB name")
+	// flag.StringVar(&dbUser, "database-user", "root", "DB User name")
+	// flag.StringVar(&dbPass, "database-pass", "jalapeno", "DB User's password")
 
 	flag.StringVar(&unicastprefixV4, "unicast_prefix_v4", "unicast_prefix_v4", "unicast_prefix_v4 Collection name, default: \"unicast_prefix_v4\"")
 	flag.StringVar(&unicastprefixV6, "unicast_prefix_v6", "unicast_prefix_v6", "unicast_prefix_v6 Collection name, default: \"unicast_prefix_v6\"")
